e32b1ceb264a9aefe8375e91b3a599001e5b18e9
heixiaopengyou <163804893+heixiaopengyou@users.noreply.github.com>
2024-05-27 12:47:40 +0200

    Create modelLossPruning.m

---
+++ Helper_Functions/modelLossPruning.m
@@ -0,0 +1,5 @@
+function [loss,pruningGradient,pruningActivations,netGradients,state] = modelLossPruning(prunableNet, X, T)
+[dlYPred,state,pruningActivations] = forward(prunableNet,X);
+loss = crossentropy(dlYPred,T);
+[pruningGradient,netGradients] = dlgradient(loss,pruningActivations,prunableNet.Learnables);
+end
